148d94b65ae7da7113c888292f6286b7a89a4c84
Mahyar Mahdavi <Mahyar@Mahyar24.com>
2023-05-03 15:05:01 +0330

    bloody damn bug!

--- v2conf/__main__.py
+++ v2conf/__main__.py
@@ -33,7 +33,7 @@ from zoneinfo import ZoneInfo
 from .configs import make_conf, write_conf
 from .health import rank_outbounds
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Mahyar Mahdavi"
 __email__ = "Mahyar@Mahyar24.com"
 __license__ = "GPLv3"
@@ -45,7 +45,7 @@ def check_requirements() -> None:
     """
     Assert if user is running the script with root privileges.
     """
-    assert os.getuid() != 0, "You must have super user permissions to run this program."
+    assert os.getuid() == 0, "You must have super user permissions to run this program."
 
 
 def tehran_time(*_, **__) -> time.struct_time:
